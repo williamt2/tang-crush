ef042bab72dc512fc96581d69315cd59b672c628
williamt2 <86770020+williamt2@users.noreply.github.com>
2021-07-01 15:15:29 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    introTitle: 'GETT àCXCSOFDJFDS;FFVDGKJ!',
+    introTitle: 'GETT àCXCSOFDJFDS;
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
